4d218cd010725a0df35088ff63304519868f928c
Divya_K <57564600+emxkd@users.noreply.github.com>
2020-11-21 22:49:52 +0530

    Update menu.py

--- menu.py
+++ menu.py
@@ -67,41 +67,41 @@ dnf sysop1():
         if i != 'y':
           break
 
-      elif int(ch) == 5:
+      elif ch == 5:
         name = input("enter package name:")
         os.system("yum install {} -y".format(name))
         i = input("do you want to continue on webserver menu [y/n]:\t")
         if i != 'y':
           break
 
-      elif int(ch) == 6:
+      elif ch == 6:
         name = input ("enter package name:")
         os.system("yum info {}".format(name))
 	i = input("do you want to continue on webserver menu [y/n]:\t")
         if i != 'y':
           break
   
-      elif int(ch) == 7:
+      elif ch == 7:
         num = input ("enter the service name:")
         os.system("systemctl status {}".format(num))
 	i = input("do you want to continue on webserver menu [y/n]:\t")
         if i != 'y':
           break
   
-      elif int(ch) == 8:
+      elif ch == 8:
         os.system("rpm -qa")
 	i = input("do you want to continue on webserver menu [y/n]:\t")
         if i != 'y':
           break
   
-      elif int(ch) == 9:
+      elif ch == 9:
         name = input ("enter software name:")
         os.system("yum info {}".format(name))
 	i = input("do you want to continue on webserver menu [y/n]:\t")
         if i != 'y':
           break
   
-      elif int(ch) == 10:
+      elif ch == 10:
         new_uswer = input ("enter name of the new user:")
         os.system("sudo useradd {}".format(new_user))
         os.system("id -u {}".format(new_user))
@@ -109,7 +109,7 @@ dnf sysop1():
         if i != 'y':
           break
   
-      elif int(ch) == 11:
+      elif ch == 11:
         name = input ("name the user:")
         os.system("sudo userdel {}".format(name))
 	i = input("do you want to continue on webserver menu [y/n]:\t")
@@ -164,38 +164,38 @@ dnf sysop2():
       Press 14: To stop a service
       Press 15: Back to main menu
       """)
-      ch = int(input("Enter your choice:\t"))
+      ch1 = int(input("Enter your choice:\t"))
       os.system("tput setaf 15")
 
-      if ch == 1:
+      if ch1 == 1:
         ip = input("Enter the ip of the system:\t")
         os.system("ssh {} date".format(ip))
 	i = input("do you want to continue on webserver menu [y/n]:\t")
         if i != 'y':
           break
 	
-      elif ch == 2:
+      elif ch1 == 2:
         ip = input("Enter the ip of the system:\t")
         os.system("ssh {} cal".format(ip))
 	i = input("do you want to continue on webserver menu [y/n]:\t")
         if i != 'y':
           break
 	
-      elif ch == 3:
+      elif ch1 == 3:
         ip = input("Enter the ip of the system:\t")
         os.system("ssh {} gnome-control-center".format(ip))
         i = input("do you want to continue on webserver menu [y/n]:\t")
         if i != 'y':
           break
 
-      elif ch == 4:
+      elif ch1 == 4:
         ip = input("Enter the ip of the system:\t")
         os.system("ssh {} lscpu".format(ip))
 	i = input("do you want to continue on webserver menu [y/n]:\t")
         if i != 'y':
           break
 
-      elif int(ch) == 5:
+      elif ch1 == 5:
         ip = input("Enter the ip of the system:\t")
         name = input("enter package name:")
         os.system("ssh {} yum install {} -y".format(ip, name))
@@ -203,7 +203,7 @@ dnf sysop2():
         if i != 'y':
           break
 
-      elif int(ch) == 6:
+      elif ch1 == 6:
         ip = input("Enter the ip of the system:\t")
         name = input ("enter package name:")
         os.system("ssh {} yum info {}".format(ip, name))
@@ -211,7 +211,7 @@ dnf sysop2():
         if i != 'y':
           break
   
-      elif int(ch) == 7:
+      elif ch1 == 7:
         ip = input("Enter the ip of the system:\t")
         num = input ("enter the service name:")
         os.system("ssh {} systemctl status {}".format(ip, num))
@@ -219,14 +219,14 @@ dnf sysop2():
         if i != 'y':
           break
   
-      elif int(ch) == 8:
+      elif ch1 == 8:
         ip = input("Enter the ip of the system:\t")
         os.system("ssh {} rpm -qa".format(ip))
 	i = input("do you want to continue on webserver menu [y/n]:\t")
         if i != 'y':
           break
   
-      elif int(ch) == 9:
+      elif ch1 == 9:
         ip = input("Enter the ip of the system:\t")
         name = input ("enter software name:")
         os.system("ssh {} yum info {}".format(ip, name))
@@ -234,7 +234,7 @@ dnf sysop2():
         if i != 'y':
           break
   
-      elif int(ch) == 10:
+      elif ch1 == 10:
         ip = input("Enter the ip of the system:\t")
         new_uswer = input ("enter name of the new user:")
         os.system("ssh {} sudo useradd {}".format(ip, new_user))
@@ -243,7 +243,7 @@ dnf sysop2():
         if i != 'y':
           break
   
-      elif int(ch) == 11:
+      elif ch1 == 11:
         ip = input("Enter the ip of the system:\t")
         name = input ("name the user:")
         os.system("ssh {} sudo userdel {}".format(ip, name))
@@ -251,7 +251,7 @@ dnf sysop2():
         if i != 'y':
           break
     
-      elif ch == 12: 
+      elif ch1 == 12: 
         ip = input("Enter the ip of the system:\t")
         name=input("name your directory :")
         os.system("ssh {} mkdir /{}".format(ip, name))
@@ -259,7 +259,7 @@ dnf sysop2():
         if i != 'y':
           break
 	
-      elif ch == 13:
+      elif ch1 == 13:
         ip = input("Enter the ip of the system:\t")
         name=input("name of the service :")
         os.system("ssh {} systemctl start {}".format(ip, name))
@@ -267,7 +267,7 @@ dnf sysop2():
         if i != 'y':
           break
 	
-      elif ch == 14:
+      elif ch1 == 14:
         ip = input("Enter the ip of the system:\t")
         name=input("name of the service :")
         os.system("ssh {} systemctl stop {}".format(ip, name))
@@ -275,7 +275,7 @@ dnf sysop2():
         if i != 'y':
           break
       
-      elif ch == 15:
+      elif ch1 == 15:
         break
 	
 dnf docker1():
@@ -403,74 +403,82 @@ dnf docker2():
       os.system("tput setaf 15")
 
       if di == 1:
-        with open("/etc/tum.repos.d/baseforyum.repo", "w") as d:
-            d.write("[cde1]\nbaseurl = file:///run/media/root/RHEL-8-0-0-BaseOS-x86_64/AppStream\ngpgcheck=0\n\n[cde2]\nbaseurl =file:///run/media/root/RHEL-8-0-0-BaseOS-x86_64/BaseOS\ngpgcheck=0\n")
-        os.system("sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm")
-        os.system("sudo yum update")
-        os.system("yum -y install docker-ce --nobest")
-        os.system("systemctl start docker")
-        os.system("systemctl enable docker")
+        ip = input("Enter the ip of the system:\t")
+        os.system("ssh {} sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm".format(ip))
+        os.system("ssh {} sudo yum update".format(ip))
+        os.system("ssh {} yum -y install docker-ce --nobest".format(ip))
+        os.system("ssh {} systemctl start docker".format(ip))
+        os.system("ssh {} systemctl enable docker".format(ip))
         i = input("do you want to continue on docker menu [y/n]:\t")
         if i != 'y':
           break
 
       elif di == 2:
-        os.system("docker image ls")
+        ip = input("Enter the ip of the system:\t")
+        os.system("ssh {} docker image ls".format(ip))
         i = input("do you want to continue on docker menu [y/n]:\t")
         if i != 'y':
           break
   
       elif di == 3:
-        name = input ("enter image name with version:")
-        os.system("docker pull {}".format(name))
+        ip = input("Enter the ip of the system:\t")
+        name = input ("enter image name with version(name:version):")
+        os.system("ssh {} docker pull {}".format(ip, name))
         i = input("do you want to continue on docker menu [y/n]:\t")
         if i != 'y':
           break
 
       elif di == 4:
+        ip = input("Enter the ip of the system:\t")
         name = input ("enter image name: ")
-        os.system("docker push {}".format(name))
+        os.system("ssh {} docker push {}".format(ip, name))
         i = input("do you want to continue on docker menu [y/n]:\t")
         if i != 'y':
           break
 	
       elif di == 5:
+        ip = input("Enter the ip of the system:\t")
         name = input ("enter image name:")
-        os.system("docker image rm {}".format(name))
+        os.system("ssh {} docker image rm {}".format(name))
         i = input("do you want to continue on docker menu [y/n]:\t")
         if i != 'y':
           break
   
       elif di == 6:
+        ip = input("Enter the ip of the system:\t")
         name = input ("enter image name:")
-        os.system("docker build -t {}".format(name))
+        os.system("ssh {} docker build -t {}".format(name))
         i = input("do you want to continue on docker menu [y/n]:\t")
         if i != 'y':
           break
   
       elif di == 7:
-        img_name = input ("enter name of the image:")
+        ip = input("Enter the ip of the system:\t")
+        img_name = input ("name the image:")
         port = input ("enter port:")
         con_name = input ("enter container name:")
-        os.system("docker container run --name {} -p {} {}".format(con_name, port, img_name))
+        os.system("ssh {} docker container run --name {} -p {} {}".format(ip, con_name, port, img_name))
         i = input("do you want to continue on docker menu [y/n]:\t")
         if i != 'y':
           break
   
       elif di == 8:
-        os.system("docker network ls")
+        ip = input("Enter the ip of the system:\t")
+        os.system("ssh {} docker network ls".format(ip))
         i = input("do you want to continue on docker menu [y/n]:\t")
         if i != 'y':
           break
   
       elif di == 9:
-        os.system("docker container ls")
+        ip = input("Enter the ip of the system:\t")
+        os.system("ssh {} docker container ls".format(ip))
         i = input("do you want to continue on docker menu [y/n]:\t")
         if i != 'y':
           break
   
       elif di == 10:
-        os.system("docker container rm -f $(docker ps -aq)")
+        ip = input("Enter the ip of the system:\t")
+        os.system("ssh {} docker container rm -f $(docker ps -aq)".format(ip))
         i = input("do you want to continue on docker menu [y/n]:\t")
         if i != 'y':
           break
@@ -478,5 +486,6 @@ dnf docker2():
       elif di == 11:
          break
 	
-       
+	
+
 
